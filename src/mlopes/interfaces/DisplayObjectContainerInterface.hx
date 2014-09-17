b93835753fc7920b3988a792b0213ef9eb091a2b
Marco Lopes <marco.paulo.lopes@gmail.com>
2014-09-17 13:33:53 +0100

    Add DisplayObjectContainerInterface

---
+++ src/mlopes/interfaces/DisplayObjectContainerInterface.hx
@@ -0,0 +1,27 @@
+package mlopes.interfaces;
+
+import flash.display.DisplayObject;
+import flash.geom.Point;
+
+
+interface DisplayObjectContainerInterface
+{
+    public var mouseChildren:Bool;
+    public var numChildren:Int;
+    public var tabChildren:Bool;
+
+    public function addChild(child:DisplayObject):DisplayObject;
+    public function addChildAt(child:DisplayObject, index:Int):DisplayObject;
+    public function areInaccessibleObjectsUnderPoint(point:Point):Bool;
+    public function contains(child:DisplayObject):Bool;
+    public function getChildAt(index:Int):DisplayObject;
+    public function getChildByName(name:String):DisplayObject;
+    public function getChildIndex(child:DisplayObject):Int;
+    public function getObjectsUnderPoint(point:Point):Array<DisplayObject>;
+    public function removeChild(child:DisplayObject):DisplayObject;
+    public function removeChildAt(index:Int):DisplayObject;
+    public function removeChildren(beginIndex:Int, endIndex:Int):Void;
+    public function setChildIndex(child:DisplayObject, index:Int):Void;
+    public function swapChildren(child1:DisplayObject, child2:DisplayObject):Void;
+    public function swapChildrenAt(index1:Int, index2:Int):Void;
+}
--- src/mlopes/interfaces/InteractiveObjectInterface.hx
+++ src/mlopes/interfaces/InteractiveObjectInterface.hx
@@ -6,5 +6,6 @@ interface InteractiveObjectInterface
     public var doubleClickEnabled:Bool;
     public var mouseEnabled:Bool;
     public var needsSoftKeyboard:Bool;
+
     public function requestSoftKeyboard():Bool;
 }
